ec52f3981267b08513e18022fbabb11c739ea2c6
skinito <jp.garcia@live.fr>
2023-05-14 17:55:46 +0000

    Add Monerium EURe support

--- contracts/EURMock.sol
+++ contracts/EURMock.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 /// @custom:security-contact good@juglas.name
 contract EURMock is ERC20 {
-    constructor() ERC20("GoodEuro", "gEUR") {
+    constructor() ERC20("EURMock", "EURm") {
         _mint(msg.sender, 10000 * 10 ** decimals());
     }
 
