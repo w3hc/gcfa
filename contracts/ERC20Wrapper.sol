c3110219bde2b75463946bf3e96236cb37eb6295
Bertrand Juglas <b@juglas.name>
2023-05-17 12:40:29 +0200

    Revert "Return 0 decimals (#36)"
    This reverts commit 17f2db2271c3badbb8d520ddb8390f28cfb25a72.

--- contracts/ERC20Wrapper.sol
+++ contracts/ERC20Wrapper.sol
@@ -35,8 +35,14 @@ abstract contract ERC20Wrapper is ERC20 {
     /**
      * @dev See {ERC20-decimals}.
      */
-    function decimals() public view virtual override returns (uint8) { 
-        return 0;
+    function decimals() public view virtual override returns (uint8) {
+        try IERC20Metadata(address(underlying)).decimals() returns (
+            uint8 value
+        ) {
+            return value;
+        } catch {
+            return super.decimals();
+        }
     }
 
     /**
