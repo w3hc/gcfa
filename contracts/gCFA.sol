50e49d84720ba94cc584b0ea087a9f5f5450b8ae
shloyem <39220943+Shloyem@users.noreply.github.com>
2023-05-27 14:33:26 +0300

    Add initial gooddollar whitelisted check for depositing addresses

--- contracts/gCFA.sol
+++ contracts/gCFA.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./ERC20Wrapper.sol";
+import "./Interfaces.sol";
 
 /**
  * @dev gCFA ERC20 token wrapped from Euro stablecoin
@@ -11,11 +12,30 @@ import "./ERC20Wrapper.sol";
  * Users can withdraw gCFA and receive a matching number of Euro stablecoin.
  */
 contract gCFA is ERC20Wrapper {
-    constructor(
-        IERC20 wrappedToken,
-        address recoveryAddress,
-        uint256 rate
-    ) ERC20Wrapper("Good CFA", "gCFA", wrappedToken, recoveryAddress, rate) {
-        return;
-    }
+	INameService public nameService;
+
+	constructor(
+		IERC20 wrappedToken,
+		address recoveryAddress,
+		uint256 rate,
+		INameService _nameService
+	) ERC20Wrapper("Good CFA", "gCFA", wrappedToken, recoveryAddress, rate) {
+		nameService = _nameService;
+		return;
+	}
+
+	function depositFor(
+		address account,
+		uint16 amountEUR
+	) public override returns (bool) {
+		require(
+			IIdentity(nameService.getAddress("IDENTITY")).isWhitelisted(msg.sender),
+			"UBIScheme: not whitelisted"
+		);
+		return super.depositFor(account, amountEUR);
+	}
+
+	function setNameService(INameService _nameService) external {
+		nameService = _nameService;
+	}
 }
