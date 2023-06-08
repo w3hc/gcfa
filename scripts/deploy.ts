602f9f81c62a5eb20ad682a99d5af886caba77f2
shloyem <39220943+Shloyem@users.noreply.github.com>
2023-06-08 17:29:59 +0300

    Added Identity and Nameservice mocks to allow easy add/removal from whitelist + prettier

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -13,10 +13,10 @@ async function main() {
   const [recovery] = await ethers.getSigners();
   recoveryAddress = recovery.address;
   switch (network.name) {
-    case "alfajores": 
-    euroAddress = process.env.EURM_ALFAJORES_CONTRACT_ADDRESS;
-    recoveryAddress = process.env.CELO_TESTNET_DAO_ADDRESS;
-    break;
+    case "alfajores":
+      euroAddress = process.env.EURM_ALFAJORES_CONTRACT_ADDRESS;
+      recoveryAddress = process.env.CELO_TESTNET_DAO_ADDRESS;
+      break;
     case "goerli":
       // deploy EUR
       const EUR = await ethers.getContractFactory("EURMock");
@@ -28,7 +28,7 @@ async function main() {
       );
       console.log("\nBlock number:", msg(receipt.blockNumber));
       euroAddress = eur.address;
-      
+
       try {
         console.log("\nEURMock contract Etherscan verification in progress...");
         await eur.deployTransaction.wait(6);
@@ -36,7 +36,7 @@ async function main() {
           network: network.name,
           address: eur.address,
           constructorArguments: [],
-          contract: "contracts/EURMock.sol:EURMock",
+          contract: "contracts/mocks/EURMock.sol:EURMock",
         });
         console.log("Etherscan verification done. ✅");
       } catch (error) {
@@ -49,7 +49,6 @@ async function main() {
     case "celo":
       euroAddress = process.env.CEUR_CONTRACT_ADDRESS;
       break;
-    
     case "gnosis":
       euroAddress = process.env.EURE_CONTRACT_ADDRESS
       break;
@@ -66,24 +65,24 @@ async function main() {
   await gcfa.deployed();
   console.log("\ngCFA contract deployed at", msg(gcfa.address), "✅");
   switch (network.name) {
-    case "alfajores": 
+    case "alfajores":
     case "celo":
     case "gnosis":
     case "goerli":
-  try {
-    console.log("\ngCFA contract Etherscan verification in progress...");
-    await gcfa.deployTransaction.wait(6);
-    await hre.run("verify:verify", {
-      network: network.name,
-      address: gcfa.address,
-      constructorArguments: [euroAddress, recoveryAddress, rate],
-      contract: "contracts/gCFA.sol:gCFA",
-    });
-    console.log("Etherscan verification done. ✅");
-  } catch (error) {
-    console.error(error);
+      try {
+        console.log("\ngCFA contract Etherscan verification in progress...");
+        await gcfa.deployTransaction.wait(6);
+        await hre.run("verify:verify", {
+          network: network.name,
+          address: gcfa.address,
+          constructorArguments: [euroAddress, recoveryAddress, rate],
+          contract: "contracts/gCFA.sol:gCFA",
+        });
+        console.log("Etherscan verification done. ✅");
+      } catch (error) {
+        console.error(error);
+      }
   }
-}
 }
 
 main().catch((error) => {
