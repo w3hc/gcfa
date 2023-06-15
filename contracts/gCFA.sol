5fec67cb0f791eeb9f01ede31ebfef06c3d72f3e
julienbrg <julien@strat.cc>
2023-06-15 22:35:24 +0200

    fix override

--- contracts/gCFA.sol
+++ contracts/gCFA.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./ERC20Wrapper.sol";
-import "./Interfaces.sol";
 
 /**
  * @dev gCFA ERC20 token wrapped from Euro stablecoin
@@ -12,30 +11,19 @@ import "./Interfaces.sol";
  * Users can withdraw gCFA and receive a matching number of Euro stablecoin.
  */
 contract gCFA is ERC20Wrapper {
-	INameService public nameService;
-
 	constructor(
 		IERC20 wrappedToken,
 		address recoveryAddress,
 		uint256 rate,
-		INameService _nameService
-	) ERC20Wrapper("Good CFA", "gCFA", wrappedToken, recoveryAddress, rate) {
-		nameService = _nameService;
-		return;
-	}
-
-	function depositFor(
-		address account,
-		uint16 amountEUR
-	) public override returns (bool) {
-		require(
-			IIdentity(nameService.getAddress("IDENTITY")).isWhitelisted(msg.sender),
-			"UBIScheme: not whitelisted"
-		);
-		return super.depositFor(account, amountEUR);
-	}
-
-	function setNameService(INameService _nameService) external {
-		nameService = _nameService;
-	}
+		INameService nameService
+	)
+		ERC20Wrapper(
+			"Good CFA",
+			"gCFA",
+			wrappedToken,
+			recoveryAddress,
+			rate,
+			nameService
+		)
+	{}
 }
